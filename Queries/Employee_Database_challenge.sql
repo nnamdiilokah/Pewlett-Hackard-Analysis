6d606e3959f258b54a34b39b7daf8e32be2daeb8
Nnamdi Ilokah <nnamdi.ilokah@mail.utoronto.ca>
2022-10-10 15:22:16 -0400

    updating mentorship eligibility query

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -37,21 +37,6 @@ e.birth_date,
 d.from_date, 
 d.to_date,
 t.title
--- INTO
-FROM employees AS e
-LEFT JOIN dept_emp AS d ON e.emp_no = d.emp_no
-LEFT JOIN titles AS t ON e.emp_no = t.emp_no
-WHERE d.to_date = '9999-01-01' AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
-ORDER BY e.emp_no;
-
--- Creating Mentorship Eligibiltiy Table
-SELECT DISTINCT ON (e.emp_no) e.emp_no, 
-e.first_name, 
-e.last_name, 
-e.birth_date,
-d.from_date, 
-d.to_date,
-t.title
 INTO mentorship_eligibility
 FROM employees AS e
 LEFT JOIN dept_emp AS d ON e.emp_no = d.emp_no
